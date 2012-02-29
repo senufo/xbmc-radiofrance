e2d590d0071e1d9372e41204c628cbe29fe8a762
senufo <fce.valeins@libertysurf.fr>
2012-02-29 14:38:43 +0100

    Cleanning code

--- default.py
+++ default.py
@@ -1,44 +1,45 @@
+"""
+Fix bug in stream mp3 : MP3codec reading
+in transcode this stream with ffmpeg
 
+Author : Senufo 2012 (c)
+"""
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui,httplib,htmllib,xbmc
-import subprocess, time
+import xbmcplugin, xbmcgui, xbmc
+import subprocess, time, sys
 
-PLUGIN              ='plugin.audio.franceinfo'
-VERSION             ='0.1'
-USER_AGENT          ='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
-#mplayer -vo null -ao pcm:file=/tmp/fi.mp3 http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3
-#ffmpeg -i /tmp/fifo /tmp/foo.ogg
+# magic; id of this plugin's instance - cast to integer
+thisPlugin = int(sys.argv[1])
+#add on id - name of addon director y
+_id = 'plugin.audio.franceinfo'
+#resources direc tory
+_resdir = "special://home/addons/" + _id + "/resources"
+#add our library to python search path
+sys.path.append ( _resdir + "/lib/" )
+
 url = 'http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3'
 #url = 'http://radioblagon-serveur3.zik.dj:8010/'
-#xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(url)
-mplayer_cmd = "/usr/bin/mplayer"
-mplayer_arg = "-vo null -ao pcm:file=/tmp/fifo http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3"
 ffmpeg_cmd = '/usr/bin/ffmpeg'
 ffmpeg_arg = '-i /tmp/fifo /tmp/foo.ogg'
-print "%s, %s " % (mplayer_cmd,mplayer_arg)
-return_code = subprocess.check_call([mplayer_cmd, mplayer_arg] , shell=True)
-print "Return code = %s " % return_code
-return_code = subprocess.call([ffmpeg_cmd, ffmpeg_arg])
-#return_code = subprocess.call([ffmpeg_cmd, '-y','-i',
-#                               'http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3',
-#                              '/tmp/foo.ogg', '&'])
-return_code = subprocess.Popen(['/usr/bin/ffmpeg', '-y','-i',
-                               'http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3',
-                              '/tmp/fifo.ogg'])
-PID = '%s' % return_code.pid
+objProcess = subprocess.Popen(['/usr/bin/ffmpeg', '-y', '-i',
+                'http://mp3.live.tv-radio.com/franceinfo/all/franceinfo.mp3',
+                '/tmp/fifo.ogg'])
+PID = '%s' % objProcess.pid
 
-print "Return code 2 = %s " % return_code
+print "Return code 2 = %s " % objProcess
 time.sleep(10)
 listitem = xbmcgui.ListItem('FRANCE INFO')
-listitem.setInfo('music', {'Title': 'France Info','genre': 'news'})
+listitem.setInfo('music', {'Title': 'France Info', 'genre': 'news'})
 listitem.setIconImage('icon.png')
-listitem.setThumbnailImage('/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/icon.png')
+listitem.setThumbnailImage(
+    '/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/icon.png')
 listitem.setProperty( "Fanart_Image",
-                     '/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/fanart.jpg' )
+    '/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/fanart.jpg' )
 #xbmc.Player( xbmc.PLAYER_CORE_MPLAYER ).play('/tmp.fifo.ogg', listitem, True)
 xbmc.Player().play('/tmp/fifo.ogg', listitem, True)
 time.sleep(10)
-return_code = subprocess.Popen(['/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/json.py',PID])
+objProcess = subprocess.Popen(
+    ['/home/henri/Sources/Video/xbmc/addons/github/plugin.audio.franceinfo/json.py',PID])
 #xbmc.Player().play('/tmp.fifo.ogg', listitem, True)
 #xbmc.Player().play('/tmp/fifo.ogg')
 print 'FIN FRANCEINFO'
--- json.py
+++ json.py
@@ -1,49 +1,30 @@
 #!/usr/bin/python
-from pprint import pprint
-import simplejson as json
+"""
+Kill ffmpeg when xbmc player stop
+"""
 
-import socket, random, sys, time
+import simplejson as json
+import socket, sys, time
 import subprocess
 
-for arg in sys.argv: 
-    print arg
-
-print "arg = %i " % int(sys.argv[1])
+#Get ffmpeg PID
 PID = sys.argv[1]
-req = {'jsonrpc':'2.0', 'id':str(random.randint(0,100)), 'method':'JSONRPC.Ping'}
+#Connection au serveur XBMC json PORT 9090
 s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost",9090))
-#s.send(json.dumps(req))
-#reponse = s.recv(1024)
-#print reponse
-#req = {'jsonrpc':'2.0', 'method':'Player.OnPlay'}
-#req = {"jsonrpc":"2.0","id":1,"method":"System.GetInfoLabels","params":{"labels":["MusicPlayer.Cover"]}}
-#{"jsonrpc":"2.0","method":"Player.OnPlay","params":{"data":{"item":{"type":"movie"},"player":{"playerid":1,"speed":1},"title":""},"sender":"xbmc"}}
-
-#req = {"jsonrpc":"2.0","id":str(random.randint(0,100)),"method":"Player.OnPlay"}
-#req = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id":"10"}
-req = {"method":"Player.GetItem","id":2097173,"jsonrpc":
-       "2.0","params":{"playerid":0,"properties":["title","artist","albumartist","genre","year",
-                                                  "rating","album","track","duration","comment",
-                                                  "lyrics","playcount","fanart","runtime",
-                                                  "streamdetails","thumbnail","file","artistid","albumid"]}}
+s.connect(("localhost", 9090))
+#requete json pour savoir si le player est en fonction
+req = {"method":"Player.GetItem", "id":2097173, "jsonrpc":
+       "2.0", "params":{"playerid":0,"properties":
+                         ["title", "artist", "albumartist", "genre", "year",
+                          "rating", "album", "track", "duration", "comment",
+                          "lyrics", "playcount", "fanart", "runtime",
+                          "streamdetails", "thumbnail", "file", "artistid",
+                          "albumid"]}}
 s.send(json.dumps(req))
 reponse = s.recv(2048)
-print reponse
-print "===================="
-#json_data=open('json_data')
-
 data = json.loads(reponse)
-pprint(data)
-#json_data.close()
-print "===================="
-print data
-print "===================="
-for key in data:
-    print "kay = %s, data = %s " % (key,data[key])
-#print "==> %s " % data["result"]["item"]["file"]
-#print "==> %s " % data["result"]["item"]["label"]
-#data["om_points"]
+
+#Boucle pour attendre la fin du player et tuer ffmpeg
 while True:
     s.send(json.dumps(req))
     reponse = s.recv(2048)
@@ -57,11 +38,6 @@ while True:
         
     except:
         print "C'est fini"
-        subprocess.Popen(['kill','-9',PID])
+        subprocess.Popen(['kill', '-9', PID])
         exit()
     time.sleep(10)
-
-print reponse[0]
-f = open('/tmp/workfile', 'w')
-f.write(reponse)
-f.close()
